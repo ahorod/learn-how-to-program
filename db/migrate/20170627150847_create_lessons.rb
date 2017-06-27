235683d3730657760677c0f717a0c39992f8f473
Max Reichman <reichmanmax@gmail.com>
2017-06-27 09:00:34 -0700

    Set up the project with a lesson object.

---
+++ db/migrate/20170627150847_create_lessons.rb
@@ -0,0 +1,10 @@
+class CreateLessons < ActiveRecord::Migration[5.1]
+  def change
+    create_table :lessons do |t|
+      t.column :name, :string
+      t.column :content, :text
+
+      t.timestamps
+    end
+  end
+end
